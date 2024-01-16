01e09e61cf5882f1d39fb397885c9f1c19a102dc
Nathan La Barbera <nlabarbera@mahalobanking.com>
2024-01-16 11:31:52 -0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "LogSnag",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LogSnag",
             targets: ["LogSnag"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LogSnag"),
         .testTarget(
--- Sources/LogSnag/LogSnag.swift
+++ Sources/LogSnag/LogSnag.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct LogSnag {
+    
+    static var apiKey = "my-api-key"
+    static var projectName = "the-project"
+
+    public static func configure(with apiKey: String, projectName: String) {
+        LogSnag.apiKey = apiKey
+        LogSnag.projectName = projectName
+    }
+}
